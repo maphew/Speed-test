381b21ebc090c595b8378d2917fc2b54aad4914b
matt wilkie <maphew@gmail.com>
2014-09-05 13:13:29 -0700

    reformat foxidrive for better understanding

--- stats/foxidrive_parse.bat
+++ stats/foxidrive_parse.bat
@@ -1,8 +1,9 @@
 REM foxidrive: http://stackoverflow.com/a/25657687/14420
 @echo off
 (
+ echo Source,Destination,Total bytes,MB per min
  for /f "usebackq tokens=1,* delims==" %%a in ("input_data.txt") do (
-   if not defined header echo Source,Destination,Total bytes,MB per min & set header=1
-   for /f "tokens=*" %%c in ("%%b") do if "%%a"=="MB per min " (set/p=""%%c""<nul&echo() else (set/p=""%%c","<nul)
+   for /f "tokens=*" %%c in ("%%b") do (
+    if "%%a"=="MB per min " (set/p=""%%c""<nul&echo() else (set/p=""%%c","<nul)
  )
 )>"%~n0_result.csv"
