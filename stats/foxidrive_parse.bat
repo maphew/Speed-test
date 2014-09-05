226c504206b6e85f88cd04eb39f79dfa6ce3a837
matt wilkie <maphew@gmail.com>
2014-09-05 14:24:20 -0700

    remove unneeded experiments

--- stats/foxidrive_parse.bat
+++
@@ -1,9 +0,0 @@
-REM foxidrive: http://stackoverflow.com/a/25657687/14420
-@echo off
-(
- echo Source,Destination,Total bytes,MB per min
- for /f "usebackq tokens=1,* delims==" %%a in ("input_data.txt") do (
-   for /f "tokens=*" %%c in ("%%b") do (
-    if "%%a"=="MB per min " (set/p=""%%c""<nul&echo() else (set/p=""%%c","<nul)
- )
-)>"%~n0_result.csv"
