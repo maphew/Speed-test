ad57ef2603624cb4e005ec2553ead8cb07c1f22e
matt wilkie <maphew@gmail.com>
2014-09-05 14:31:08 -0700

    xxcopylog_to_stats: add usage message

--- stats/xxcopylog_to_stats.bat
+++ stats/xxcopylog_to_stats.bat
@@ -1,29 +1,45 @@
 @echo OFF
+:: Generate statistics from an XXCCOPY logfile
 :: Thank you dbenham: http://stackoverflow.com/a/25657306/14420 
 setlocal enableDelayedExpansion
+
+if "%1"=="" goto :Usage
 
 set "input=%1"
 set "output=%~n1_stats.csv"
 
-set "row="
-set "header="
-set "begin="
-set "first="
-(
-  for /f "usebackq tokens=1* delims==" %%A in ("%input%") do for /f "tokens=*" %%C in ("%%B") do (
-    if "!begin!" equ "%%A" (
-      if not defined first (
-        set first=1
-        echo !header:~1!
+
+:Main
+  set "row="
+  set "header="
+  set "begin="
+  set "first="
+  (
+    for /f "usebackq tokens=1* delims==" %%A in ("%input%") do for /f "tokens=*" %%C in ("%%B") do (
+      if "!begin!" equ "%%A" (
+        if not defined first (
+          set first=1
+          echo !header:~1!
+        )
+        echo !row:~1!
+        set "row="
       )
-      echo !row:~1!
-      set "row="
+      set "row=!row!,"%%C""
+      if not defined first for /f "delims=" %%H in ("%%A") do (
+        if not defined begin set "begin=%%A"
+        set "header=!header!,"%%~nH""
+      )
     )
-    set "row=!row!,"%%C""
-    if not defined first for /f "delims=" %%H in ("%%A") do (
-      if not defined begin set "begin=%%A"
-      set "header=!header!,"%%~nH""
-    )
-  )
-  echo !row:~1!
-)>"%output%"
+    echo !row:~1!
+  )>"%output%"
+  echo. --- "%input%" stats saved to "%output%"
+  goto :eof
+
+
+:Usage
+  echo.
+  echo.  Usage:  
+  echo.
+  echo.     %~n0 [path\to\input_logfile]
+  echo.
+  goto :eof
