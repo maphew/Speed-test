11ad6415e11a494fb596aec04b584fb5e2f18f5a
matt wilkie <maphew@gmail.com>
2014-09-08 10:58:38 -0700

    Add debug and annotation comments

--- stats/xxcopylog_to_stats.bat
+++ stats/xxcopylog_to_stats.bat
@@ -1,6 +1,9 @@
 @echo OFF
 :: Generate statistics from an XXCCOPY logfile
-:: Thank you dbenham: http://stackoverflow.com/a/25657306/14420 
+:: Thank you dbenham: http://stackoverflow.com/a/25657306/14420
+::
+:: Debugging: uncomment lower case "rem" lines
+::
 setlocal enableDelayedExpansion
 
 if "%1"=="" goto :Usage
@@ -18,23 +21,58 @@ goto :eof
   set "begin="
   set "first="
   (
-    for /f "usebackq tokens=1* delims==" %%A in ("%input%") do for /f "tokens=*" %%C in ("%%B") do (
-      if "!begin!" equ "%%A" (
-        if not defined first (
-          set first=1
-          echo !header:~1!
+    for /f "usebackq tokens=1* delims==" %%A in ("%input%") do (
+      REM At this point %%A is {before =} and %%B is {after =}
+      REM or Field & Value 
+      rem echo --FIELD: %%A
+      rem echo --VALUE: %%B
+      
+      for /f "tokens=*" %%C in ("%%B") do (
+          REM %%C is B%% with any leading whitespace stripped 
+          REM (an effect of "tokens=*")
+          rem echo --VAL-2: %%C
+           
+          if "!begin!" equ "%%A" (
+            REM Emit header row if this is the first time through outer loop
+            if not defined first (
+              set first=1
+              echo !header:~1!
+            )
+            echo !row:~1!
+            set "row="
+          )
+          
+          REM This adds leading comma on 1st loop run 
+          set "row=!row!,"%%C""
+
+          REM On first loop...
+          if not defined first for /f "delims=" %%H in ("%%A") do (
+            REM %%H is header row column name
+            rem echo --COLUMN: %%H
+            
+            if not defined begin set "begin=%%A"
+            
+            REM "%%~nH" is to strip trailing whitespace
+            rem echo --COLUMN    : "%%H"
+            rem echo --COLUMN~nH : "%%~nH"
+
+            rem echo --HEADER-OLD: !header!
+            rem echo --HEADER-NEW: !header!,"%%~nH""
+            set "header=!header!,"%%~nH""
+          )
         )
-        echo !row:~1!
-        set "row="
-      )
-      set "row=!row!,"%%C""
-      if not defined first for /f "delims=" %%H in ("%%A") do (
-        if not defined begin set "begin=%%A"
-        set "header=!header!,"%%~nH""
-      )
     )
+    REM Emit data row. "~1" strips the leading comma left by
+    REM something in previous loop.
+    rem echo --DATA-RAW  : !row!
+    rem echo --DATA-STRIP: !row:~1!
     echo !row:~1!
-  )>"%output%"
+  
+  REM to debug: uncomment 1st line and comment 2nd
+  REM (prints to console instead of file)
+  )
+  rem )>"%output%"
+  
   echo. --- "%input%" stats saved to "%output%"
   goto :eof
 
