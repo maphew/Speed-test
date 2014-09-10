6faeb1542e22c623ac2d1ad322728870382e9f7e
matt wilkie <maphew+github@gmail.com>
2014-09-09 23:46:18 -0700

    half baked, with progress (read extended commit)
    before going through lines 1by1, separate into records bounded by "===="
    and an empty line. (Took a long time to figure out that '' means empty
    line to the record...yield def, while '\r\n' and similar do not!)

--- stats/parse-log.py
+++ stats/parse-log.py
@@ -12,29 +12,76 @@
 import csv
 import sys
 
+d = {}
 def main():
     f = open(r"b:\github\Speed-test\stats\GEOMATT\server2drobo_diff-little_files.log")
     for line in f.readlines():
+        """ Extract xxcopy & windows version numbers, date & time from:
 
-        # Extract xxcopy & windows version numbers, date & time from:
-        #
-        #   XXCOPY ver 2.97.5   2008-10-22 09:12:22   Windows Ver 5.2.3790 Service Pack 2
-        #
+            XXCOPY ver 2.97.5   2008-10-22 09:12:22   Windows Ver 5.2.3790 Service Pack 2
+        """
+##        d = {}
+        # line of ===... marks new log entry, so start new dict
+        if line[0:6] == "======":
+            d['end_of_record'] = False
 
         if line[0:6] == "XXCOPY":
-            d = {}
             discard, xxcv_date_time, winver = line.lower().split("ver")
-            xxcv, run_date, run_time, discard = xxver_date_time.split()
+            xxcv, run_date, run_time, discard = xxcv_date_time.split()
             d["xxcopy version"] = xxcv
             d["date"] = run_date
             d["time"] = run_time
+##            d["windows version"] = winver
+##            print(line)
+##            print(d)
 
         if " = " in line:
             field, value = line.split(" = ")
             d[field.strip()] = value.strip()
 
-    if 'd' in locals():
-            print(d)
+        print (d.keys())
+        if 'Exit code' in d:
+            d['end_of_record'] = True
+            write_csv(d)
+##                print(d)
+
+def recordsFromFile(inputFile):
+    """ http://stackoverflow.com/questions/8131197/efficiently-parsing-a-large-text-file-in-python """
+    record = ''
+##    terminator = '=' * 20
+    terminator = '' #
+    for line in inputFile:
+        if line.startswith('=') and record.endswith(terminator):
+            yield record
+            record = ''
+        record += line
+    yield record
+
+
+def write_csv(d):
+##    f = open(sys.argv[1], 'wt')
+    # Adapted from http://pymotw.com/2/csv/
+    testfile = r'b:\github\Speed-test\stats\from-py.csv'
+    f = open(testfile, 'w')
+    try:
+        fieldnames = sorted(d.keys())
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        headers = dict( (n,n) for n in fieldnames )
+        writer.writerow(headers)
+        for i in range(10):
+            writer.writerow(d)
+    finally:
+        f.close()
+
+##    print (open(testfile, 'rt').read())
+##    print open(sys.argv[1], 'rt').read()
 
 if __name__ == '__main__':
-    main()
+##    main()
+    inputFile = open(r"b:\github\Speed-test\stats\GEOMATT\server2drobo_diff-little_files.log")
+    for record in recordsFromFile(inputFile):
+        for line in (record.splitlines()):
+            if line[0:6] == "XXCOPY":
+                print(line)
+
+        # Do stuff
