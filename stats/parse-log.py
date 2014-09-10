435388730e4b3efb824b62b51cc54930536a93fd
matt wilkie <maphew@gmail.com>
2014-09-10 11:11:13 -0700

    rolled up batch of changes, read extended
    Doc intent of recordsFromFile(); Try matching newline, carriage return,
    braces for end of record (none of these work);
    
    Fix bug in write_csv(): was repeating the same row many times
    
    Move parse_xxcopy and parse_fields into their own functions.

--- stats/parse-log.py
+++ stats/parse-log.py
@@ -11,77 +11,91 @@
 #!/usr/bin/env python
 import csv
 import sys
-
-d = {}
-def main():
-    f = open(r"b:\github\Speed-test\stats\GEOMATT\server2drobo_diff-little_files.log")
-    for line in f.readlines():
-        """ Extract xxcopy & windows version numbers, date & time from:
-
-            XXCOPY ver 2.97.5   2008-10-22 09:12:22   Windows Ver 5.2.3790 Service Pack 2
-        """
-##        d = {}
-        # line of ===... marks new log entry, so start new dict
-        if line[0:6] == "======":
-            d['end_of_record'] = False
-
-        if line[0:6] == "XXCOPY":
-            discard, xxcv_date_time, winver = line.lower().split("ver")
-            xxcv, run_date, run_time, discard = xxcv_date_time.split()
-            d["xxcopy version"] = xxcv
-            d["date"] = run_date
-            d["time"] = run_time
-##            d["windows version"] = winver
-##            print(line)
-##            print(d)
-
-        if " = " in line:
-            field, value = line.split(" = ")
-            d[field.strip()] = value.strip()
-
-        print (d.keys())
-        if 'Exit code' in d:
-            d['end_of_record'] = True
-            write_csv(d)
-##                print(d)
+import re
 
 def recordsFromFile(inputFile):
-    """ http://stackoverflow.com/questions/8131197/efficiently-parsing-a-large-text-file-in-python """
+    """
+    For us, a record is everthing between the line beginning with '=' and ends
+    with a closing brance and blank line.
+
+    <<<record begins:
+
+        ===============================================================================
+        ...snip...
+         Exit code             = 0 (No error, Successful operation)
+
+    /record ends>>>
+
+    It would be more robust to match " Exit code ... )\r\n". Perhaps a later
+    enhancement.
+
+    http://stackoverflow.com/questions/8131197/efficiently-parsing-a-large-text-file-in-python
+    """
     record = ''
 ##    terminator = '=' * 20
-    terminator = '' #
+    terminator = '\n'
     for line in inputFile:
-        if line.startswith('=') and record.endswith(terminator):
+        if line.startswith('===') and record.endswith(terminator):
             yield record
             record = ''
         record += line
     yield record
 
+def parse_xxcopy_line(line, d):
+    """ Extract xxcopy & windows version numbers, date & time from:
 
-def write_csv(d):
-##    f = open(sys.argv[1], 'wt')
+        XXCOPY ver 2.97.5   2008-10-22 09:12:22   Windows Ver 5.2.3790 Service Pack 2
+    """
+    discard, xxcv_date_time, winver = line.lower().split("ver")
+    xxcv, run_date, run_time, discard = xxcv_date_time.split()
+    d["xxcopy version"] = xxcv
+    d["date"] = run_date
+    d["time"] = run_time
+    d["windows version"] = winver
+    return d
+
+def parse_field(line, d):
+    """ Extract regular field & value from:
+
+            Destination directory = "R:\speed-test\big_files\"
+            Directories processed = 1
+            Total data in bytes   = 527,331,269
+            ...
+    """
+    if " = " in line:
+        field, value = line.split(" = ")
+        d[field.strip()] = value.strip()
+    return d
+
+def write_csv(dic, csvfile):
     # Adapted from http://pymotw.com/2/csv/
-    testfile = r'b:\github\Speed-test\stats\from-py.csv'
-    f = open(testfile, 'w')
-    try:
-        fieldnames = sorted(d.keys())
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        headers = dict( (n,n) for n in fieldnames )
-        writer.writerow(headers)
-        for i in range(10):
-            writer.writerow(d)
-    finally:
-        f.close()
-
-##    print (open(testfile, 'rt').read())
-##    print open(sys.argv[1], 'rt').read()
+    f = open(csvfile, 'w')
+    print("in csv writer DIC %s" % dic['time'])
+    print("in csv writer D %s" % d['time'])
+    fieldnames = sorted(dic.keys())
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    headers = dict( (n,n) for n in fieldnames )
+    writer.writerow(headers)
+    writer.writerow(dic)
+    f.close()
 
 if __name__ == '__main__':
 ##    main()
-    inputFile = open(r"b:\github\Speed-test\stats\GEOMATT\server2drobo_diff-little_files.log")
+##    inputFile = open(r"b:\github\Speed-test\stats\GEOMATT\server2drobo_diff-little_files.log")
+    inputFile = open(r"D:\speed-test\stats\ENV-Y209103\local2NAS-local-user-raid10_diff-little_files.log")
+    csvfile = r'b:\github\Speed-test\stats\from-py.csv'
     for record in recordsFromFile(inputFile):
+        print("\n***********************************\n %s" % record)
+        d = {} # new dict for each record
         for line in (record.splitlines()):
             if line[0:6] == "XXCOPY":
                 print(line)
-
-        # Do stuff
+                parse_xxcopy_line(line,d)
+##                print(d['time'])
+            elif " = " in line:
+##                print(line)
+                parse_field(line,d)
+##            print(d['time'])
+        write_csv(d, csvfile)
+        print(d.keys())
+        print(d['time'])
